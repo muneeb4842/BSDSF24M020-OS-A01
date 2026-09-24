e19992cedcbf1e346b625986e09c5b89b93766ce
muneeb <muneeb4842@gmail.com>
2026-09-24 09:10:18 +0000

    Implemented multi-file build system and code

---
+++ include/myfilefunctions.h
@@ -0,0 +1,6 @@
+#ifndef MYFILEFUNCTIONS_H
+#define MYFILEFUNCTIONS_H
+
+int count_lines(const char *filename);
+
+#endif
---
+++ include/mystrfunctions.h
@@ -0,0 +1,7 @@
+#ifndef MYSTRFUNCTIONS_H
+#define MYSTRFUNCTIONS_H
+
+int my_strlen(const char *str);
+void my_strcpy(char *dest, const char *src);
+
+#endif
