1503ae3f5372e1406da1b66a61bda59b8e13f695
muneeb <muneeb4842@gmail.com>
2026-09-24 09:29:36 +0000

    Fixed code and updated files for multifile build

--- include/mystrfunctions.h
+++ include/mystrfunctions.h
@@ -1,16 +1,7 @@
-#include "mystrfunctions.h"
+#ifndef MYSTRFUNCTIONS_H
+#define MYSTRFUNCTIONS_H
 
-int my_strlen(const char *str) {
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    return len;
-}
+int my_strlen(const char *str);
+void my_strcpy(char *dest, const char *src);
 
-void my_strcpy(char *dest, const char *src) {
-    int i = 0;
-    while ((dest[i] = src[i]) != '\0') {
-        i++;
-    }
-}
+#endif
